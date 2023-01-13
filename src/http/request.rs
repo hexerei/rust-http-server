d2ce980d0c5a75e1f4715fbb4a14031f296001d0
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2023-01-13 14:29:05 +0100

    start with tryfrom for request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 /*
 GET /user?id=10 HTTP/1.1\r\n
@@ -10,4 +11,11 @@ pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
-}        
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
