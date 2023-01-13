b879edcc071bb597d0a69a7bb2f94aa7deb4e969
Daniel Vorhauer <daniel@hexerei.net>
2023-01-13 23:24:35 +0100

    handle public path and return file contents

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,6 +1,21 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
-pub struct WebsiteHandler;
+use std::fs;
+
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
@@ -8,16 +23,19 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(
                     StatusCode::Ok,
-                    Some("<h1>Welcome</h1>".to_string())
+                    self.read_file("index.html")
                 ),
                 "/hello" => Response::new(
                     StatusCode::Ok,
-                    Some("<h1>Hello</h1>".to_string())
+                    self.read_file("hello.html")
                 ),
-                _ => Response::new(
-                    StatusCode::NotFound,
-                    None
-                ),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(
+                        StatusCode::NotFound,
+                        None
+                    ),
+                }
             }
             _ => Response::new(StatusCode::NotFound, None)
         }
